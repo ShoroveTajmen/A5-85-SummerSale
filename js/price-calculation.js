d5250b08e0fb8ed20fe1d6674c0cbb16e85c2253
ShoroveTajmen <sorovitajmen@gmail.com>
2023-08-20 14:19:19 +0600

    added calculation

--- js/price-calculation.js
+++ js/price-calculation.js
@@ -1,31 +1,3 @@
-//select first card
-// document.getElementById('card-1').addEventListener('click', function(){
-//     const productOneName = document.getElementById('productName1').innerText;
-//     console.log(productOneName);
-//     const price1 = document.getElementById('price1').innerText;
-//     console.log(price1);
-
-//     const calculationEntry = document.getElementById('calculation-entry');
-//     const count = calculationEntry.childElementCount;
-//     const p = document.createElement('p');
-//     p.innerHTML = `${count + 1}. ${productOneName}`;
-//     calculationEntry.appendChild(p);
-
-// })
-// document.getElementById('card-2').addEventListener('click', function(){
-//     const product2ndName = document.getElementById('productName2').innerText;
-//     console.log(product2ndName);
-//     const price2 = document.getElementById('price2').innerText;
-//     console.log(price2);
-
-//     const calculationEntry = document.getElementById('calculation-entry');
-//     const count = calculationEntry.childElementCount;
-//     const p = document.createElement('p');
-//     p.innerHTML = `${count + 1}. ${product2ndName}`;
-//     calculationEntry.appendChild(p);
-
-// })
-
 //function for click card
 let total = 0;
 function cardClick(target) {
@@ -34,13 +6,33 @@ function cardClick(target) {
   const calculationEntry = document.getElementById("calculation-entry");
   const count = calculationEntry.childElementCount;
   const p = document.createElement("p");
-  p.classList.add("pt-2", "pl-4", "font-semibold");
+  p.classList.add("pt-2", "pl-4", "font-semibold", "text-lg");
   p.innerHTML = `${count + 1}. ${productName}`;
   calculationEntry.appendChild(p);
 
   const productPriceString = target.childNodes[7].innerText.split(" ")[0];
   const productPrice = parseFloat(productPriceString);
   total = total + productPrice;
-  const totaPrice = total.toFixed(2);
-  console.log(totaPrice);
+  const totalPrice = total.toFixed(2);
+
+  if (totalPrice > 200) {
+    const discountPercentage = 20;
+    const discountAmount = (discountPercentage / 100) * totalPrice;
+    const discountAmountRounded = discountAmount.toFixed(2);
+
+    //get discount element
+    const discount = document.getElementById("discountPrice");
+    discount.innerText = discountAmountRounded;
+
+    const discountedPrice = totalPrice - discountAmount;
+    const discountedPriceRounded = discountedPrice.toFixed(2);
+
+    //get total element
+    const Total = document.getElementById('total');;
+    Total.innerText = discountedPriceRounded;
+  }
+
+  //get totalPrice element
+  const Price = document.getElementById("Price");
+  Price.innerText = totalPrice;
 }
